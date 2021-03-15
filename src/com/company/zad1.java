d2a2e12060921eade8fd784c69f557c969a34c9b
Jakub16 <73905068+Jakub16@users.noreply.github.com>
2021-03-15 16:34:37 +0100

    Initial commit

---
+++ src/com/company/zad1.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+
+
+public class zad1 {
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        int a = myObj.nextInt();
+
+
+        int b = myObj.nextInt();
+
+        float area = a * b;
+        System.out.println(area);
+    }
+}
---
+++ src/com/company/zad2.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class zad2 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
