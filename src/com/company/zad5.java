2eddccecb006e41bb9cbd0610587482f557cf1f6
Jakub16 <73905068+Jakub16@users.noreply.github.com>
2021-03-16 20:08:22 +0100

    Initial commit

--- src/com/company/zad5.java
+++ src/com/company/zad5.java
@@ -3,6 +3,7 @@ package com.company;
 import java.util.Scanner;
 
 public class zad5 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj miesiąc:");
--- src/com/company/zad6.java
+++ src/com/company/zad6.java
@@ -1,7 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class zad6 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        float a = scanner.nextFloat();
+        float b = scanner.nextFloat();
+        float c = scanner.nextFloat();
 
 
     }
